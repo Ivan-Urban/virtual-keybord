705f5b6d6b5ef7237d6877c98f753e08d2acf863
Ivan-Urban <ivan.urbanovich.00@gmail.com>
2022-05-05 20:11:09 +0300

    feat: add textarea

--- src/index.js
+++ src/index.js
@@ -2,3 +2,7 @@ const title = document.createElement('span');
 title.textContent = 'Виртуальная клавиатура';
 title.classList.add('title');
 document.body.append(title);
+
+const input = document.createElement('textarea');
+input.classList.add('textarea');
+document.body.append(input);
